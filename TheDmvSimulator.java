963f7d5406ee7817897b818352777624e2dddf6d
Zbroadbent2 <zachbroadbent04@gmail.com>
2024-03-05 18:45:13 -0500

    Step 1 Functionality

--- TheDmvSimulator.java
+++ TheDmvSimulator.java
@@ -1,6 +1,21 @@
+import java.util.Random;
 
 public class TheDmvSimulator {
-    public static void main (String args[]){
+    public static void main(String args[]) {
+        System.out.println("Welcome to the DMV!");
 
+        Random random = new Random();
+        int userNumber = random.nextInt(100) + 1;
+
+        System.out.println("Your number is " + userNumber + ". Please wait until your number is called.");
+        
+        for (int i = userNumber + 1; i <= 100; i++) {
+            System.out.println("Calling number " + i);
+        }
+        for (int i = 1; i < userNumber; i++) {
+        }
+
+        System.out.println("Calling number " + userNumber);
+        System.out.println("Sorry, you don't have the required paperwork ");
     }
 }
